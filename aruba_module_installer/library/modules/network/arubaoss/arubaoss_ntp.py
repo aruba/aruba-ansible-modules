3c84c920123fc66373a8772c0eeff3b57a50cf3d
tiffany-l-chiapuzio-wong <tiffany.l.chiapuzio-wong@hpe.com>
2020-02-23 21:12:15 -0800

    fixes module oobm support

--- aruba_module_installer/library/modules/network/arubaoss/arubaoss_ntp.py
+++ aruba_module_installer/library/modules/network/arubaoss/arubaoss_ntp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (c) 2019 Hewlett Packard Enterprise Development LP
+# Copyright (c) 2020 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -186,7 +186,7 @@ EXAMPLES = '''
       - name: Remove all NTP traps
         arubaoss_ntp:
           command: "config_ntp"
-          trap_value: 
+          trap_value:
             - enable: False
               trap: "all"
 '''
@@ -549,14 +549,9 @@ def config_ntp_ipv4addr(module):
 
     # Configuring OOBM
     if params['use_oobm'] is True:
-        check_presence = get_config(module, "/oobm")
-        if check_presence:
-            use_oobm = {}
-            use_oobm = {'oobm': params['use_oobm']}
-            data[ip4_addr][ip4_addr_ref].update(use_oobm)
-        else:
-            return {'msg': 'Device does not have OOBM port',
-                    'changed': False, 'failed': True}
+        use_oobm = {}
+        use_oobm = {'oobm': params['use_oobm']}
+        data[ip4_addr][ip4_addr_ref].update(use_oobm)
 
     # URIs
     url = "/config/ntp/" + server + "/" + ip4_addr
@@ -602,11 +597,18 @@ def config_ntp_ipv4addr(module):
     # When address is in host name format, "check" in run_commands will not work.
     # so adding the additional check to verify idempotency
     if ip4_addr == "ASCII-STR" and diffseen == False and method != 'DELETE':
-        return {'msg': 'Configuration already exist',
+        return {'msg': 'Configuration already exists',
                 'changed': False, 'failed': False}
     result = run_commands(module, url, data, method, check=check_url)
-    return result
-
+    try:
+        if result['status'] == 400:
+            newdata = json.loads(result['body'])
+            return {'msg': newdata['config_error'][0]['error'],
+                'changed': False, 'failed': True}
+        else :
+            return result
+    except:
+        return result
 
 """
 -------
